5ef5b807fc61ccaf5aa8450d8c1c096067093557
NSlocal <nadyaillustrationartist@gmail.com>
2026-08-27 00:44:16 +0700

    Update SafetyCoreApp.kt

--- app/src/main/java/com/google/android/safetycore/SafetyCoreApp.kt
+++ app/src/main/java/com/google/android/safetycore/SafetyCoreApp.kt
@@ -1,9 +1,3 @@
 package com.google.android.safetycore
-
 import android.app.Application
-
-class SafetyCoreApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
+class SafetyCoreApp : Application()

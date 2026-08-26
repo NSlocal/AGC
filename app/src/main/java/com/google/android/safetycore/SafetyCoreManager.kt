6799ccf0028c06464b8576dc81e4e4d36fb25a67
NSlocal <nadyaillustrationartist@gmail.com>
2026-08-26 16:59:09 +0700

    Update SafetyCoreManager.kt

--- app/src/main/java/com/google/android/safetycore/SafetyCoreManager.kt
+++ app/src/main/java/com/google/android/safetycore/SafetyCoreManager.kt
@@ -2,14 +2,7 @@ package com.google.android.safetycore
 
 import android.content.Context
 
-class SafetyCoreManager(private val context: Context) {
-
-    fun setGlobalEnabled(enabled: Boolean): Boolean {
-        SafetyCoreService.setEnabled(context, enabled)
-        return true
-    }
-
-    fun isGlobalEnabled(): Boolean {
-        return SafetyCoreService.isEnabled(context)
-    }
+class SafetyCoreManager(private val ctx: Context) {
+    fun setEnabled(enabled: Boolean) = SafetyCoreService.setEnabled(ctx, enabled)
+    fun isEnabled() = SafetyCoreService.isEnabled(ctx)
 }

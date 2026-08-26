2aca3c5a87ff16c411cfdc50f4aae8cedd62f5a7
NSlocal <nadyaillustrationartist@gmail.com>
2026-08-26 23:54:08 +0700

    Update SafetyCoreApp.kt

--- app/src/main/java/com/google/android/safetycore/SafetyCoreApp.kt
+++ app/src/main/java/com/google/android/safetycore/SafetyCoreApp.kt
@@ -1,21 +1,9 @@
 package com.google.android.safetycore
 
 import android.app.Application
-import android.util.Log
 
 class SafetyCoreApp : Application() {
-    companion object {
-        lateinit var instance: SafetyCoreApp
-            private set
-    }
-
     override fun onCreate() {
         super.onCreate()
-        instance = this
-        try {
-            Log.d("SafetyCoreApp", "✅ App Initialized")
-        } catch (e: Exception) {
-            Log.e("SafetyCoreApp", "⚠️ Init error: ${e.message}")
-        }
     }
 }

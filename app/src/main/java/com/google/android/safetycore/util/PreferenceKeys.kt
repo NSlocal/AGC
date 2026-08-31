7406ad48fe1469913176b1e9b2ff8f3588f597a7
NSlocal <nadyaillustrationartist@gmail.com>
2026-08-31 22:11:41 +0700

    Create PreferenceKeys.kt

---
+++ app/src/main/java/com/google/android/safetycore/util/PreferenceKeys.kt
@@ -0,0 +1,13 @@
+package com.google.android.safetycore.util
+object PreferenceKeys {
+    const val DARK_MODE = "dark_mode"
+    const val AUTO_START_FPS = "auto_start_fps"
+    const val AUTO_START_BOOST = "auto_start_boost"
+    const val BG_OPACITY = "bg_opacity"
+    const val TEXT_SIZE = "text_size"
+    const val SHOW_CPU = "show_cpu"
+    const val SHOW_GPU = "show_gpu"
+    const val SHOW_TEMP = "show_temp"
+    const val SHOW_BATTERY = "show_battery"
+    const val REFRESH_RATE = "refresh_rate"
+}

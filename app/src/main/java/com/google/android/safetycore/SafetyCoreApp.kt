623410d9bfa746dcd8d6f9cc4ea0cf0da2c07237
NSlocal <nadyaillustrationartist@gmail.com>
2026-08-31 22:07:35 +0700

    Update SafetyCoreApp.kt

--- app/src/main/java/com/google/android/safetycore/SafetyCoreApp.kt
+++ app/src/main/java/com/google/android/safetycore/SafetyCoreApp.kt
@@ -1,5 +1,18 @@
 package com.google.android.safetycore
-
 import android.app.Application
+import android.util.Log
 
-class SafetyCoreApp : Application()
+class SafetyCoreApp : Application() {
+    companion object {
+        lateinit var instance: SafetyCoreApp
+            private set
+        const val TAG = "SafetyCorePro"
+        var isDarkMode = true
+        var refreshRate = 120f
+    }
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        Log.d(TAG, "SafetyCore Pro Initialized")
+    }
+}

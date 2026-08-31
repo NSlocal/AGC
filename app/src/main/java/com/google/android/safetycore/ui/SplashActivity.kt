8accb400fe3c24032a9574f6f73e76d32c2fdfd2
NSlocal <nadyaillustrationartist@gmail.com>
2026-08-31 22:08:08 +0700

    Create SplashActivity.kt

---
+++ app/src/main/java/com/google/android/safetycore/ui/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.google.android.safetycore.ui
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.safetycore.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, SettingsActivity::class.java))
+            finish()
+        }, 1200)
+    }
+}

a6692bac1885f86f9030328a68e2e40c0e7f045c
NSlocal <nadyaillustrationartist@gmail.com>
2026-09-01 21:17:36 +0700

    Update SafetyCoreManager.kt

--- app/src/main/java/com/google/android/safetycore/SafetyCoreManager.kt
+++ app/src/main/java/com/google/android/safetycore/SafetyCoreManager.kt
@@ -1,8 +1,12 @@
 package com.google.android.safetycore
-
 import android.content.Context
-
-class SafetyCoreManager(private val ctx: Context) {
-    fun setEnabled(enabled: Boolean) = SafetyCoreService.setEnabled(ctx, enabled)
-    fun isEnabled() = SafetyCoreService.isEnabled(ctx)
+import android.content.Intent
+import android.net.Uri
+import com.google.android.safetycore.content.ContentScannerService
+object SafetyCoreManager {
+    fun scanImage(context: Context, uri: Uri) {
+        val intent = Intent(context, ContentScannerService::class.java)
+        intent.putExtra(ContentScannerService.EXTRA_URI, uri)
+        context.startForegroundService(intent)
+    }
 }

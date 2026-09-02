86522fd0882b5677306034bfe77c20f623786944
NSlocal <nadyaillustrationartist@gmail.com>
2026-09-02 21:42:48 +0700

    Create SystemMonitor.kt

---
+++ app/src/main/java/com/google/android/safetycore/monitor/SystemMonitor.kt
@@ -0,0 +1,13 @@
+package com.google.android.safetycore.monitor
+
+import android.content.Context
+
+class SystemMonitor(private val context: Context) {
+    fun start() {
+        // Mulai pemantauan
+    }
+
+    fun stop() {
+        // Berhenti pemantauan
+    }
+}

44079491368d3d867cb14ed7d89b824c1fc7d33e
NSlocal <nadyaillustrationartist@gmail.com>
2026-09-02 22:57:59 +0700

    Update SettingsActivity.kt

--- app/src/main/java/com/google/android/safetycore/ui/SettingsActivity.kt
+++ app/src/main/java/com/google/android/safetycore/ui/SettingsActivity.kt
@@ -1,12 +1,10 @@
 package com.google.android.safetycore.ui
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.safetycore.R
-
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        setContentView(android.R.layout.simple_list_item_1)
     }
 }
